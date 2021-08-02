f2758553ab13a126ee56bac6a0e48c011dec8089
mrLuisFer <mrluisfeer@gmail.com>
2021-08-02 18:15:57 -0500

    fix: making the secondArgs available for all commands

--- src/commands/animeSearch/index.ts
+++ src/commands/animeSearch/index.ts
@@ -7,20 +7,15 @@ import infoEmbed from './utils/infoEmbed'
 
 export const baseUrl: string = 'https://api.jikan.moe/v3'
 
-export const anime = async ({ msg, args }: MsgWithArgs): Promise<void> => {
+export const anime = async ({ msg, args = [], arg }: MsgWithArgs): Promise<void> => {
   const randomEmbedColor: string = getRandomEmbedColor()
-  if (args.length > 0) {
-    const arg: string = args.join('-')
-    // const secondArg: string = args[1]
-    const secondArgs: string[] = args.filter((arg) => !arg.startsWith('--'))
-    const argsFiltered: string = secondArgs.join('-')
-    console.log(argsFiltered)
-
+  if (args?.length > 0) {
     const firstArg: string = args[0]
+
     if (firstArg === '--m' || firstArg === '--manga') {
-      searchManga(msg, argsFiltered, randomEmbedColor)
+      searchManga(msg, arg, randomEmbedColor)
     } else if (firstArg === '--p' || firstArg === '--people') {
-      searchChar(msg, argsFiltered, randomEmbedColor)
+      searchChar(msg, arg, randomEmbedColor)
     } else if (args[0].startsWith('--')) {
       const embed = infoEmbed(randomEmbedColor)
       msg.channel.send(embed)
--- src/types/msgArgs.d.ts
+++ src/types/msgArgs.d.ts
@@ -2,5 +2,6 @@ import { Message } from 'discord.js'
 
 export type MsgWithArgs = {
   msg: Message
-  args: string[]
+  args?: string[]
+  arg: string
 }
