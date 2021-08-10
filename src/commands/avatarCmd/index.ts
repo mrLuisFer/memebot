1105695b6966b719bdfc8a9fab67b0e9610f41dc
mrLuisFer <mrluisfeer@gmail.com>
2021-08-09 23:35:03 -0500

    feat: adding more details in avatar command with help flag

--- src/commands/avatarCmd/index.ts
+++ src/commands/avatarCmd/index.ts
@@ -1,23 +1,26 @@
 import { Message } from 'discord.js'
 import { getRandomEmbedColor } from '../../utils/embedColors'
 import { avatarEmbed } from './avatarEmbed'
+import { infoEmbed } from './infoEmbed'
 
 type Props = {
   msg: Message
   firstArg: string
 }
 
-export const avatarCmd = ({ msg, firstArg }: Props) => {
+export const avatarCmd = async ({ msg, firstArg }: Props) => {
   const embedColor: string = getRandomEmbedColor()
-  console.log(firstArg)
 
-  if (firstArg?.length > 1) {
+  if (firstArg === '--help' || firstArg === '--h' || firstArg?.startsWith('--')) {
+    msg.channel.send(infoEmbed(embedColor))
+  } else if (firstArg?.length > 1) {
     const user = msg.mentions.users.first()
     if (user !== undefined) {
       const embed = avatarEmbed({ embedColor, user })
       msg.channel.send(embed)
     } else {
-      msg.reply('Usuario no encontrado')
+      msg.reply(`Usuario **${firstArg}** no encontrado`)
+      await msg.react('😓')
     }
   } else {
     const msgAuthor = msg.author
---
+++ src/commands/avatarCmd/infoEmbed.ts
@@ -0,0 +1,21 @@
+import { MessageEmbed } from 'discord.js'
+import { getRandomGif } from '../../utils/gifs/animeGifs'
+
+export const infoEmbed = (embedColor: string): MessageEmbed => {
+  const randomAnimeGif: string = getRandomGif()
+  console.log(randomAnimeGif)
+  const embed: MessageEmbed = new MessageEmbed()
+    .setTitle('Avatar Command')
+    .setColor(embedColor)
+    .setDescription(
+      `
+  Puedes ejecutar el comando de las siguientes maneras:
+
+  \`b!avatar\` -> Retorna tu propio avatar
+
+  \`b!avatar <username>\` -> Retorna el avatar del usuario que coloques
+`.trim()
+    )
+    .setImage(randomAnimeGif)
+  return embed
+}
