089c07b03fbb1c8f0bf8f55c9cc6285ad451b9c6
mrLuisFer <mrluisfeer@gmail.com>
2021-08-25 19:26:00 -0500

    fix: rename client to bot

--- src/bot.ts
+++ src/bot.ts
@@ -1,37 +1,37 @@
-import { Client, Message } from 'discord.js'
+import { Client, Message, PartialMessage } from 'discord.js'
 import { config } from './config'
 import { presence } from './utils/botPresence'
 import { init } from './commands'
 
-export const client: Client = new Client()
+export const bot: Client = new Client()
 
-client.on('ready', () => {
-  console.log(`Bot is ready as ${client?.user?.tag}!`)
-  presence(client)
-  // handles the maximum available event or command listener
-  client.setMaxListeners(20)
+bot.on('ready', () => {
+  console.log(`Bot is ready as ${bot?.user?.tag}!`)
+  presence(bot)
+  bot.setMaxListeners(20)
 })
 
-client.on('message', (message: Message) => {
+bot.on('message', (message: Message) => {
   if (
     !message.content.startsWith(config.prefix) ||
     message.author.bot ||
-    message.author === client.user
+    message.author === bot.user
   )
     return
-
   const args: string[] = message.content.slice(config.prefix.length).trim().split(/ +/g)
   const command: string = args?.shift()?.toLowerCase() || ''
-
   console.log(`Comando -> ${command}`)
-  console.log(`Client -> ${client}`)
+  console.log(`Client -> ${bot}`)
   console.log(`Message -> ${message}`)
-
   init({ command, args, message })
 })
 
-client.on('guildMemberAdd', (member) => {
-  console.log(`New member: ${member.user}`)
+bot.on('guildMemberAdd', async (member) => {
+  console.log(member)
 })
 
-client.login(config.token)
+bot.on('messageDelete', (message: Message | PartialMessage) => {
+  console.log(message)
+})
+
+bot.login(config.token)
