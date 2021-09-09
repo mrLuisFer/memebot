972deff2d0e794e540bbabd69cc1018412cd8050
mrLuisFer <mrluisfeer@gmail.com>
2021-09-08 19:15:39 -0500

    [feat] add handler for nsfw posts

--- src/commands/funCommands/redditSearch/utils/getSubredditPosts.ts
+++ src/commands/funCommands/redditSearch/utils/getSubredditPosts.ts
@@ -1,24 +1,30 @@
 import { getCustomSubRedditData } from '../redditApiReq'
 import { getRandomNumber } from '../../redditSearch'
-import { Message } from 'discord.js'
+import { Message, TextChannel } from 'discord.js'
 import { apiEmbed } from '../apiEmbed'
 import { getRandomEmbedColor } from '../../../../utils/embedColors'
 
 type Props = {
-  msg: Message
+  msg: Message | TextChannel | any
   arg: string
 }
 
 export const getSubredditPosts = async ({ msg, arg }: Props) => {
+  const isNsfw: boolean = msg.channel.nsfw
   const embedColor: string = getRandomEmbedColor()
-  if (arg.length > 0) {
-    const results = await getCustomSubRedditData(arg)
-    const randomId: number = getRandomNumber(results)
-    const data = results[randomId]?.data
-    const embed = apiEmbed(data, embedColor)
 
-    msg.channel.send(embed)
+  if (!isNsfw) {
+    msg.reply('El contenido necesita estar en un canal nsfw')
   } else {
-    msg.reply('Coloca un subReddit valido para buscar')
+    if (arg.length > 0) {
+      const results = await getCustomSubRedditData(arg)
+      const randomId: number = getRandomNumber(results)
+      const data = results[randomId]?.data
+      const embed = apiEmbed(data, embedColor)
+
+      msg.channel.send(embed)
+    } else {
+      msg.reply('Coloca un subReddit valido para buscar')
+    }
   }
 }
