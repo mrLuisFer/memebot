b8d05de025c625e3caf8b314baa614d198a8a65d
mrLuisFer <mrluisfeer@gmail.com>
2021-09-10 07:51:07 -0500

    [fix] remove discord-canvas package because it is not maintained

--- src/commands/serverCommands/welcome/welcomeCanvaMsg.ts
+++ src/commands/serverCommands/welcome/welcomeCanvaMsg.ts
@@ -1,9 +1,1 @@
-import canvas from 'discord-canvas'
-
-export const welcomeCanvaMsg = async () => {
-  const welcomeCanvas = await canvas.Welcome()
-
-  const embed = await welcomeCanvas.setUsername('mrluisfer')
-
-  console.log(embed)
-}
+export const welcomeCanvaMsg = async () => {}
