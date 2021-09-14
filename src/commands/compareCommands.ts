4f7853dc9671dfd733840f1bc0f35725ab0ebc8f
mrLuisFer <mrluisfeer@gmail.com>
2021-09-14 16:34:46 -0500

    [feat] add details for youtube card buffer

--- src/commands/compareCommands.ts
+++ src/commands/compareCommands.ts
@@ -57,7 +57,7 @@ export const compareCommand = ({
       welcome({ msg: message, firstArg })
       break
     case 'ytc':
-      youtubeCard()
+      youtubeCard({ msg: message })
       break
     default:
       helpCommand(message)
--- src/commands/funCommands/youtubeCard/index.ts
+++ src/commands/funCommands/youtubeCard/index.ts
@@ -1,5 +1,18 @@
 import { Canvacord } from 'canvacord'
+import { Message, MessageAttachment } from 'discord.js'
 
-export const youtubeCard = () => {
-  console.log(Canvacord.youtube({ username: 'Luis', content: 'Sopmething', avatar: '' }))
+type YoutubeCardProps = {
+  msg: Message
+}
+export const youtubeCard = async ({ msg }: YoutubeCardProps) => {
+  const opts = {
+    username: msg.author.username,
+    avatar:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-kK2MFoGYgK0%2FXrW8t7lXRpI%2FAAAAAAAADZU%2FN5UA-NaVaIIg_rjl1h-zn7Dn9R4FcCBAgCLcBGAsYHQ%2Fs1600%2Fdiscord-logo.jpg&f=1&nofb=1',
+    content: `@${msg.author.id}`,
+  }
+
+  const card = await Canvacord.youtube(opts)
+  const msgAttachment = new MessageAttachment(card)
+  msg.channel.send(msgAttachment)
 }
